36811e98ad7d193c6a6cf884d46a060735044490
Александр Касьян <gumoni.33@mail.ru>
2021-11-23 20:02:54 +0300

    Added search by cities and diseases

--- VaccinationCampaignUI/Controllers/VaccinationController.cs
+++ VaccinationCampaignUI/Controllers/VaccinationController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VaccinationCampaignUI.Data;
 using VaccinationCampaignUI.Models;
+using VaccinationCampaignUI.ViewModels;
 
 namespace VaccinationCampaignUI.Controllers
 {
@@ -66,6 +68,47 @@ namespace VaccinationCampaignUI.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> NumberVaccination(int cityId, int diseaseId) 
+        {
+            var citiesNames = _context.Addresses.Select(x => x);
+            var cities = new List<SelectViewModel>();
+            
+            foreach (var item in citiesNames)
+            {
+                var actual = cities.Select(x => x.Name);
+                if (!actual.Contains(item.Locality))
+                {
+                    cities.Add(new SelectViewModel { Id = item.Id, Name = item.Locality, });
+                }
+            }
+            var diseases = await Task.Run(() => _context.Diseases.Select(x => new SelectViewModel { Id = x.Id, Name = x.NameDis }));
+
+            var model = new NumberVaccinationViewModel
+            {
+                Cities = cities.ToList(),
+                Diseases = diseases.ToList(),
+            };
+
+            if (cityId != 0 && diseaseId != 0)
+            {
+                var vaccines = _context.Vaccines.Where(x => x.DiseaseId == diseaseId).Select(x => x.Id);
+                var allVaccinations = _context.Vaccinations.Select(x => x);
+                var vaccinationsCount = allVaccinations.Where(x => vaccines.Contains(x.Id));
+
+                var city = await _context.Addresses.FirstOrDefaultAsync(x => x.Id == cityId);
+                var selectedCitiesIds = _context.Addresses.Where(x => x.Locality.Equals(city.Locality)).Select(x => x.Id);
+                var patients = _context.Patients.Where(x => selectedCitiesIds.Contains(x.Id)).Select(x => x.Id);
+
+                var countResult = await vaccinationsCount.Where(x => patients.Contains(x.PatientId.Value)).CountAsync();
+                model.Count = countResult;
+            }
+
+
+            return View(model); 
+        }
+
     }
 }
 
---
+++ VaccinationCampaignUI/ViewModels/NumberVaccinationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccinationCampaignUI.ViewModels
+{
+    public class NumberVaccinationViewModel
+    {
+        public List<SelectViewModel> Cities { get; set; }
+
+        public List<SelectViewModel> Diseases { get; set; }
+
+        public int Count { get; set; }
+    }
+}
